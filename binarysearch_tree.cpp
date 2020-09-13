f0c88670ca5a0ff11b09f8a9d960591b0ba6d220
root <root@localhost.localdomain>
2020-09-13 10:08:47 +0530

    Add

---
+++ binarysearch_tree.cpp
@@ -0,0 +1,14 @@
+/*
+Binary Search Tree:
+
+BST is a binary data structure, in which the values in the left sub-tree of every node are smaller and the values in the right sub-tree are larger. 
+*/
+
+ 
+
+
+
+
+
+
+
